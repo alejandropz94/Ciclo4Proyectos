484e26da671b6c3bc192b7c38dde8c4aced997cb
Luis Parra <parralinaresluis@gmail.com>
2021-12-12 22:16:58 -0500

    Desarrollo interfaz de actualizar proyectos

--- Frontend/src/components/proyectos/FilaTabla.js
+++ Frontend/src/components/proyectos/FilaTabla.js
@@ -52,7 +52,11 @@ function FilaTabla(props) {
             console.log(error);
         }
     }
-
+    const {id, sendDataEditar} = props;
+    const handleEditar = e => {
+        let id = e.target.getAttribute("_id");
+        sendDataEditar(id);
+    }
     return (
         proyectos.map((proyecto, index) => (
             <tr key={proyecto._id}>
@@ -60,9 +64,19 @@ function FilaTabla(props) {
                 <td>{proyecto.nombre}</td>
                 <td>{proyecto.presupuesto}</td>
                 <td>{proyecto.lider.nombre}</td>
-                <td>{proyecto.fechaInicio}</td>
+                <td>{new Date(Number(proyecto.fechaInicio)).toLocaleDateString("en-US")}</td>
                 <td>{proyecto.estado}</td>
                 <td>{proyecto.fase}</td>
+                <td>
+                    <div className="btn-group btn-group-sm">
+                        <span _id={proyecto._id} className="btn btn-warning" data-bs-toggle="modal" data-bs-target="#modalProyecto" onClick={handleEditar}>
+                            <svg _id={proyecto._id} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16" onClick={handleEditar}>
+                                <path _id={proyecto._id} d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
+                            </svg>
+                        </span>
+                    </div>
+
+                </td>
             </tr>
         ))
     );
--- Frontend/src/components/proyectos/TablaProyectos.js
+++ Frontend/src/components/proyectos/TablaProyectos.js
@@ -1,8 +1,143 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../../styles/estilos.css'
 import { Fragment } from 'react/cjs/react.production.min';
 import FilaTabla from './FilaTabla';
+import {
+    ApolloClient,
+    InMemoryCache,
+    gql
+} from "@apollo/client";
+
+import notie from 'notie';
+import 'notie/dist/notie.css';
+
 function TablaProyectos() {
+    const client = new ApolloClient({
+        uri: `${process.env.REACT_APP_API_URL}/graphql`,
+        cache: new InMemoryCache()
+    });
+    const QUERY_GETPROJECT = gql`
+    query GetProject($_id: ID) {
+        getProject(_id: $_id) {
+          _id
+          nombre
+          presupuesto
+          fechaInicio
+          fechaFin
+          estado
+          fase
+        }
+      }`;
+    const UPDATE_PROJECT = gql`
+      mutation UpdateProject($id: ID, $input: ProjectUpdate) {
+        updateProject(_id: $id, input: $input) {
+          _id
+          nombre
+          presupuesto
+          fechaInicio
+          fechaFin
+          estado
+          fase
+        }
+      }`;
+
+    const [idProyectoEditar, setIdProyectoEditar] = useState("");
+
+    function getDataEditar(val) {
+        setIdProyectoEditar(val);
+    }
+
+    useEffect(() => {
+        getProyecto();
+    }, [idProyectoEditar]);
+
+    const [nombre, setNombre] = useState("");
+    const [presupuesto, setPresupuesto] = useState();
+    const [fechaInicio, setFechaInicial] = useState("");
+    const [fechaFin, setFechaFinal] = useState("");
+    const [estado, setEstado] = useState("");
+    const [fase, setFase] = useState("");
+    const [tituloModal, setTituloModal] = useState("");
+    const [editar, setEditar] = useState(false);
+
+    const handleCerrar = e => {
+        setIdProyectoEditar("");
+        setFechaFinal("");
+        setEstado("");
+        setFase("");
+        setNombre("");
+        setPresupuesto("");
+        setFechaInicial("");
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const proyecto = {
+            presupuesto,
+            fechaInicio,
+            fechaFin,
+            estado,
+            fase
+        };
+        if (editar) {
+            client.mutate({
+                mutation: UPDATE_PROJECT,
+                variables: {
+                    id: idProyectoEditar,
+                    input: proyecto
+                }
+            }).then(result => {
+                document.querySelector('.closeModalProyecto').click();
+                notie.alert({
+                    type: 'success',
+                    text: "Proyecto actualizado correctamente",
+                });
+                setTimeout(() => { window.location.href = "/proyectos" }, 1500);
+            }).catch(error => {
+                console.log(error);
+            });
+        }
+    }
+
+    const getProyecto = async function () {
+        setEditar(true);
+        if (idProyectoEditar !== null && idProyectoEditar !== "") {
+            setTituloModal("Actualizar proyecto");
+            try {
+                client
+                    .query({
+                        query: QUERY_GETPROJECT,
+                        variables: { _id: idProyectoEditar }
+                    })
+                    .then(result => {
+                        let proyecto = result.data.getProject;
+                        setNombre(proyecto.nombre);
+                        setPresupuesto(proyecto.presupuesto);
+                        setFase(proyecto.fase);
+                        setEstado(proyecto.estado);
+
+                        let dateString = new Date(Number(proyecto.fechaInicio)).toLocaleDateString("en-US");
+                        let dateString2 = new Date(Number(proyecto.fechaFin)).toLocaleDateString("en-US");
+                        let auxDate = dateString.split("/");
+                        let auxDate2 = dateString2.split("/");
+                        setFechaInicial(auxDate[2] + "-" + auxDate[1] + "-" + auxDate[0]);
+                        setFechaFinal(auxDate2[2] + "-" + auxDate2[1] + "-" + auxDate2[0])
+                    });
+            } catch (error) {
+                console.log(error);
+            }
+        } else {
+            setTituloModal("Registrar proyecto");
+            setEditar(false);
+        }
+    }
+
+    let isDisabled = false;
+    if (fechaFin === "" || estado === "" || fase === "") {
+        isDisabled = true;
+    } else {
+        isDisabled = false;
+    }
     return (
         <Fragment>
             <div className="col-xl-10 col-md-10">
@@ -14,7 +149,7 @@ function TablaProyectos() {
                                 <button
                                     className="btn btn-success mb-3"
                                     data-bs-toggle="modal"
-                                    data-bs-target="#modalProducto"
+                                    data-bs-target="#modalProyecto"
                                 >
                                     Crear Proyecto
                                 </button>
@@ -33,13 +168,98 @@ function TablaProyectos() {
                                     <th scope="col">Lider</th>
                                     <th scope="col">F. Inicio</th>
                                     <th scope="col">Estado</th>
-                                    <th scope="col">Fase</th> 
+                                    <th scope="col">Fase</th>
+                                    <th scope="col">Acciones</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <FilaTabla></FilaTabla>
+                                <FilaTabla sendDataEditar={getDataEditar}></FilaTabla>
                             </tbody>
                         </table>
+                    </div>
+                </div>
+            </div>
+            <div className="modal fade"
+                id="modalProyecto"
+                tabindex="-1"
+                data-bs-backdrop="static"
+                data-bs-keyboard="false"
+                aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="principalBody">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="tituloModal">{tituloModal}</h5>
+                                <button
+                                    type="button"
+                                    className="btn-close closeModalProyecto"
+                                    data-bs-dismiss="modal"
+                                    aria-label="Close"
+                                    onClick={handleCerrar}
+                                ></button>
+                            </div>
+                            <form onSubmit={handleSubmit}>
+                                <div className="modal-body">
+                                    <div className="row">
+                                        <div className="mb-3">
+                                            <label htmlFor="nombre" className="form-label">Nombre:</label>
+                                            <input type="text" className="form-control" id="nombre"
+                                                onChange={e => setNombre(e.target.value)}
+                                                value={nombre} />
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="mb-3">
+                                            <label htmlFor="presupuesto" className="form-label">Presupuesto:</label>
+                                            <input type="number" className="form-control" id="presupuesto"
+                                                onChange={e => setPresupuesto(e.target.value)}
+                                                value={presupuesto} />
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col">
+                                            <label htmlFor="fechaInicio" className="form-label">Fecha inicio:</label>
+                                            <input type="date" className="form-control" id="fechaInicio"
+                                                onChange={e => setFechaInicial(e.target.value)}
+                                                value={fechaInicio} defaultValue={fechaInicio} />
+                                        </div>
+                                        <div className="col">
+                                            <label htmlFor="fechaFin" className="form-label">Fecha fin:</label>
+                                            <input type="date" className="form-control" id="fechaFin"
+                                                onChange={e => setFechaFinal(e.target.value)}
+                                                value={fechaFin} defaultValue={fechaFin} />
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col">
+                                            <label htmlFor="estado" className="form-label">Estado:</label>
+                                            <select className="form-select" onChange={e => setEstado(e.target.value)}
+                                                value={estado}>
+                                                <option disabled selected value="">Seleccione</option>
+                                                <option value="Activo">Activo</option>
+                                                <option value="Inactivo">Inactivo</option>
+                                            </select>
+                                        </div>
+                                        <div className="col">
+                                            <label htmlFor="fase" className="form-label">Fase:</label>
+                                            <select className="form-select" onChange={e => setFase(e.target.value)}
+                                                value={fase}>
+                                                <option disabled selected value="">Seleccione</option>
+                                                <option value="Iniciado">Iniciado</option>
+                                                <option value="En Desarrollo">En Desarrollo</option>
+                                                <option value="Terminado">Terminado</option>
+                                                <option value="Null">Null</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" onClick={handleCerrar} data-bs-dismiss="modal">Cerrar</button>
+                                    <input type="submit" className="btn btn-primary" disabled={isDisabled} value="Guardar" />
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
